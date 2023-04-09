160adb55da8d8134458b0b721ab2fa6b50937f38
Ntare <ntare.n.tumusiime@gmail.com>
2023-04-09 12:27:37 +0300

    password viewing and checkboxes on DB page

--- members/forms.py
+++ members/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     user_name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
-    user_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'autocomplete': 'off'}))
+    user_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'id': 'password-field', 'autocomplete': 'off'}))
